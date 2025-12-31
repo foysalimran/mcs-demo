a40eedb9209e130b4a0fd12c5b41149496a42068
sakhawat9 <sakhawathossain7969@gmail.com>
2025-12-31 16:01:27 +0600

    update

--- mcs-demo.php
+++ mcs-demo.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: MCS Demo
  * Description: A demo plugin for MCS functionality.
- * Version: 1.0
+ * Version: 1.0.1
  * Author: ThemeAtelier
  * License: GPL2
  * Text Domain: mcs-demo
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-define('MCS_DEMO_PLUGIN_VERSION', '1.0');
+define('MCS_DEMO_PLUGIN_VERSION', '1.0.1');
 define('MCS_DEMO_PLUGIN_URL', trailingslashit(plugins_url('/', __FILE__)));
 
 
@@ -112,7 +112,7 @@ function mcs_demo_shortcode_function($atts)
                                                     <div class="layouts__title">Different Button</div>
                                                 </div>
                                             </div>
-                                            <div class="md:col-span-12 mb-4 usage_example" data-show="#example_9">
+                                            <div class="col-span-12 mb-4 usage_example" data-show="#example_9">
                                                 <div class="layouts__design">
                                                     <img class="img-fliud" src="<?php echo MCS_DEMO_PLUGIN_URL . 'assets/mcs-images/buttons.jpg' ?>" alt="" />
                                                     <div class="layouts__title">Buttons</div>
@@ -879,8 +879,8 @@ function register_mcs_shortcode_styles()
     wp_register_style('icofont', plugins_url('assets/css/icofont.min.css', __FILE__));
     wp_register_style('fontawesome', plugins_url('assets/css/fontawesome-all.min.css', __FILE__));
     wp_register_style('sweetalert2', plugins_url('assets/css/sweetalert2.min.css', __FILE__));
-    wp_register_style('mcs-main', plugins_url('assets/css/mcs-main.css', __FILE__));
-    wp_register_style('styles', plugins_url('assets/css/style.css', __FILE__));
+    wp_register_style('mcs-main', plugins_url('assets/css/mcs-main.css', __FILE__), array(), MCS_DEMO_PLUGIN_VERSION, 'all');
+    wp_register_style('styles', plugins_url('assets/css/style.css', __FILE__), array(), MCS_DEMO_PLUGIN_VERSION, 'all');
 
     wp_register_script('sweetalert2', MCS_DEMO_PLUGIN_URL . 'assets/js/sweetalert2.all.min.js', array('jquery'), MCS_DEMO_PLUGIN_VERSION, true);
     wp_register_script('select-togglebutton', MCS_DEMO_PLUGIN_URL . 'assets/js/select-togglebutton.min.js', array('jquery'), MCS_DEMO_PLUGIN_VERSION, true);
