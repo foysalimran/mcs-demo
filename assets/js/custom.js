161ec40f1493a8d7534825ca346fc6de0bac5d33
sakhawat9 <sakhawathossain7969@gmail.com>
2026-04-22 17:35:57 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,3 @@
-/**
- * Table of contents
- * -----------------------------------
- * 1.Button Layout
- * DARK VERSION
- */
-
 (function ($) {
   "use strict";
   var PATH = {};
@@ -12,64 +5,111 @@
   /******************** 1.Button Layout  ********************/
   PATH.ButtonsLayout = function () {
     var layoutMenu = document.querySelector(".layouts__menu__area");
-    $(".usage_example").on("click", function () {
-      var $this = $(this);
+
+    // 🔥 SHOW FUNCTION (centralized)
+    function showExample($this) {
       var $show = $($this.attr("data-show"));
+
       if (layoutMenu) {
         layoutMenu.style.display = "none";
       }
-      $(
-        "#example_1, #example_2, #example_3, #example_4, #example_5, #example_6, #example_7, #example_8, #example_9"
-      ).hide().removeClass("vChat-show");
-      $(
-        "#example_1, #example_2, #example_3, #example_4, #example_5, #example_6, #example_7, #example_8, #example_9"
-      ).hide().removeClass("wHelp-show");
-      $(
-        "#example_1, #example_2, #example_3, #example_4, #example_5, #example_6, #example_7, #example_8, #example_9"
-      ).hide().removeClass("teleSupport-show");
 
-            $(
-        "#example_1, #example_2, #example_3, #example_4, #example_5, #example_6, #example_7, #example_8, #example_9"
-      ).hide().removeClass("mSupport-show");
+      $("#example_1, #example_2, #example_3, #example_4, #example_5, #example_6, #example_7, #example_8, #example_9")
+        .hide()
+        .removeClass("vChat-show wHelp-show teleSupport-show mSupport-show");
 
-      $show.slideDown(200, function () {
+      $show.stop(true, true).slideDown(200, function () {
         setTimeout(function () {
           if ($show.find(".example_popup")[0] !== undefined) {
-            // if it has popup
             $show.find(".example_button").trigger("click");
           }
         }, 100);
       });
+    }
+
+    // 🔥 CLICK EVENT (FIXED)
+    $(".usage_example").on("click", function () {
+      var $this = $(this);
+
+      // selected class
+      $(".usage_example.selected").removeClass("selected");
+      $this.addClass("selected");
+
+      showExample($this);
+
+      // ✅ URL update WITHOUT triggering hashchange loop
+      var id = $this.attr("data-show");
+      if (id) {
+        history.pushState(null, null, id);
+      }
     });
 
-    // TRIGGER BUTTON ON ANIMATION BUTTONS CLICK
+    // 🔥 HASH HANDLER
+    function handleHash() {
+  var hash = window.location.hash;
 
-    // CHANGE EFFECT OF THE POPUP
+  if (hash) {
+    var $targetBtn = $('.usage_example[data-show="' + hash + '"]');
+
+    if ($targetBtn.length) {
+      $(".usage_example.selected").removeClass("selected");
+      $targetBtn.addClass("selected");
+
+      showExample($targetBtn);
+
+      // ✅ scroll to #shortcode instead of top
+      setTimeout(function () {
+        var target = document.getElementById("shortcode");
+        if (target) {
+          target.scrollIntoView({
+            behavior: "smooth", // চাইলে "auto" দিতে পারো
+            block: "start"
+          });
+        }
+      }, 50);
+    }
+  }
+}
+
+    // 🔥 INITIAL LOAD
+    handleHash();
+
+    // 🔥 HASH CHANGE (manual URL change)
+    $(window).on("hashchange", function () {
+      handleHash();
+    });
+
+    // CHANGE EFFECT
     $("#change_effect, #change_effect_popup").on("change", function () {
       let current = $(this).val();
       let items = $(".example_popup");
 
-      jQuery(".example_button")[0].click();
+      $(".example_button")[0].click();
 
       items.removeClass(function (index, className) {
         return (className.match(/(^|\s)animation\S+/g) || []).join(" ");
       });
+
       items.each(function () {
         $(this).addClass("animation" + current);
       });
     });
+
     $("#change_effect_popup").trigger("change");
   };
 
+  /******************** DEVICE CHANGE ********************/
   PATH.DibasicChange = () => {
     var mobileView = document.querySelector(".mobile-view");
     var desktopView = document.querySelector(".desktop-view");
     var layoutPopup = document.querySelector(".layouts__popup");
+
     if (mobileView) {
       mobileView.addEventListener("click", () => {
         layoutPopup.classList.add("layouts__popup-width");
       });
     }
+
     if (desktopView) {
       desktopView.addEventListener("click", () => {
         layoutPopup.classList.remove("layouts__popup-width");
@@ -77,29 +117,30 @@
     }
   };
 
-  /******************** DOCUMENT READY FUNCTION ********************/
+  /******************** INIT ********************/
   $(function () {
     PATH.DibasicChange();
     PATH.ButtonsLayout();
   });
 
-  /******************** WINDOW ON LOAD FUNCTION ********************/
-  $(window).on("load", function () {});
 })(jQuery);
+
 
 /******************** DARK VERSION ********************/
 function setTheme(themeName) {
   var themeColor = document.querySelector(".theme-color");
-	if(themeColor) {
-  themeColor.classList.remove("theme-light", "night-mode");
-  localStorage.setItem("reboot_theme", themeName);
-  if (themeName !== "") {
-    themeColor.classList.add(themeName);
-  } else {
-    themeColor.classList.add("theme-light");
+  if (themeColor) {
+    themeColor.classList.remove("theme-light", "night-mode");
+    localStorage.setItem("reboot_theme", themeName);
+
+    if (themeName !== "") {
+      themeColor.classList.add(themeName);
+    } else {
+      themeColor.classList.add("theme-light");
+    }
   }
-		}
 }
+
 function toggleTheme() {
   if (localStorage.getItem("reboot_theme") === "night-mode") {
     setTheme("");
@@ -107,22 +148,20 @@ function toggleTheme() {
     setTheme("night-mode");
   }
 }
+
 (function () {
   if (localStorage.getItem("reboot_theme") === "night-mode") {
     setTheme("night-mode");
-	  var slider = document.getElementById("slider");
-	  if(slider) {
-		  slider.checked = true;
-	  }
+    var slider = document.getElementById("slider");
+    if (slider) slider.checked = true;
   } else {
     setTheme("theme-light");
     var slider = document.getElementById("slider");
-	  if(slider) {
-		  slider.checked = true;
-	  }
+    if (slider) slider.checked = true;
   }
 })();
 
+/******************** MENU TOGGLE ********************/
 function myFunction() {
   var layoutMenu = document.querySelector(".layouts__menu__area");
   if (layoutMenu.style.display === "none") {
@@ -131,16 +170,3 @@ function myFunction() {
     layoutMenu.style.display = "none";
   }
 }
-
-// Add active class to the current button (highlight it)
-var header = document.getElementById("usage_examples");
-if(header) {
-var btns = header.getElementsByClassName("usage_example");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("selected");
-    current[0].className = current[0].className.replace(" selected", "");
-    this.className += " selected";
-  });
-}
-	}
